3c97a430f388f4748c934de0d582e388211cf58f
maviva97@gmail.com <maviva97@gmail.com>
2023-10-03 12:49:33 +0300

    Initial commit

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,14 @@
+package edu.tekwill.academy.computer;
+
+import edu.tekwill.academy.computer.components.*;
+import edu.tekwill.academy.computer.peripherals.*;
+
+public class Computer {
+    Processor f =  new Processor();
+    Motherboard e = new Motherboard();
+    HardDrive g = new HardDrive();
+
+    Monitor i = new Monitor();
+    Mouse y = new Mouse();
+    Keyboard j = new Keyboard();
+}
